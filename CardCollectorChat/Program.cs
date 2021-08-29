5320c5181069435095698b339545c12abc88b45f
Tigran <abvgdesh3@gmail.com>
2021-08-29 19:31:48 +0300

    Initial commit

---
+++ CardCollectorChat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardCollectorChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
