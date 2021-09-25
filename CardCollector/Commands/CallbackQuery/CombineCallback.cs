03f42b9ee5f93f66af951925d2d6e0a81944d207
Tigran <abvgdesh3@gmail.com>
2021-09-25 23:03:26 +0300

    bug fix

--- CardCollector/Commands/CallbackQuery/CombineCallback.cs
+++ CardCollector/Commands/CallbackQuery/CombineCallback.cs
@@ -11,22 +11,27 @@ namespace CardCollector.Commands.CallbackQuery
         protected override string CommandText => Command.combine;
         public override async Task Execute()
         {
+            var selectedSticker = User.Session.SelectedSticker;
             var combineCount = User.Session.GetCombineCount();
             if (combineCount == Constants.COMBINE_COUNT)
                 await MessageController.AnswerCallbackQuery(User, CallbackQueryId, Messages.cant_combine, true);
             else
             {
-                if (combineCount + User.Session.SelectedSticker.Count > Constants.COMBINE_COUNT)
+                if (combineCount + selectedSticker.Count > Constants.COMBINE_COUNT)
                 {
-                    User.Session.SelectedSticker.Count = Constants.COMBINE_COUNT - combineCount;
+                    selectedSticker.Count = Constants.COMBINE_COUNT - combineCount;
                     await MessageController.AnswerCallbackQuery(User, CallbackQueryId, $"{Messages.combine_added_only} " +
-                        $"{User.Session.SelectedSticker.Count}{Text.items}", true);
+                        $"{selectedSticker.Count}{Text.items}", true);
                 }
-
-                if (!User.Session.CombineList.ContainsKey(User.Session.SelectedSticker.Md5Hash))
-                    User.Session.CombineList.Add(User.Session.SelectedSticker.Md5Hash, User.Session.SelectedSticker);
-                else
-                    User.Session.CombineList[User.Session.SelectedSticker.Md5Hash].Count = User.Session.SelectedSticker.Count;
+                if (User.Session.CombineList.ContainsKey(selectedSticker.Md5Hash))
+                {
+                    var combineSticker = User.Session.CombineList[selectedSticker.Md5Hash];
+                    if (selectedSticker.MaxCount < combineSticker.Count + selectedSticker.Count)
+                        User.Session.CombineList[selectedSticker.Md5Hash].Count = selectedSticker.Count;
+                    else
+                        User.Session.CombineList[selectedSticker.Md5Hash].Count += selectedSticker.Count;
+                }
+                else User.Session.CombineList.Add(selectedSticker.Md5Hash, selectedSticker);
             }
             await new BackToCombine(User, Update).Execute();
         }
