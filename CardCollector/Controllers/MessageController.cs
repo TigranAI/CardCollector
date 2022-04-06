22d0605922054957c44470d273a74faf18f2bc03
Tigran <abvgdesh3@gmail.com>
2022-04-06 21:10:24 +0300

    Split controllers

--- CardCollector/Controllers/MessageController.cs
+++ CardCollector/Controllers/MessageController.cs
@@ -2,27 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using CardCollector.Commands;
-using CardCollector.Commands.CallbackQueryHandler;
-using CardCollector.Commands.ChosenInlineResultHandler;
-using CardCollector.Commands.InlineQueryHandler;
-using CardCollector.Commands.MessageHandler;
-using CardCollector.Commands.MyChatMemberHandler;
-using CardCollector.Commands.PreCheckoutQueryHandler;
-using CardCollector.Database;
-using CardCollector.Database.EntityDao;
+using CardCollector.Database.Entity;
 using CardCollector.Resources;
 using CardCollector.Resources.Enums;
 using CardCollector.Session.Modules;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineQueryResults;
 using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.Payments;
 using Telegram.Bot.Types.ReplyMarkups;
-using User = CardCollector.Database.Entity.User;
 
 namespace CardCollector.Controllers
 {
@@ -30,89 +20,6 @@ namespace CardCollector.Controllers
 
     public static class MessageController
     {
-        public static async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken ct)
-        {
-            LogOutJson(update);
-            try
-            {
-                var executor = update.Type switch
-                {
-                    UpdateType.Message => await MessageHandler.Factory(update),
-                    UpdateType.CallbackQuery => await CallbackQueryHandler.Factory(update),
-                    UpdateType.MyChatMember => await MyChatMemberHandler.Factory(update),
-                    UpdateType.InlineQuery => await InlineQueryHandler.Factory(update),
-                    UpdateType.ChosenInlineResult => await ChosenInlineResultHandler.Factory(update),
-                    UpdateType.PreCheckoutQuery => await PreCheckoutQueryHandler.Factory(update),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                try
-                {
-                    await executor.PrepareAndExecute();
-                }
-                catch (Exception e)
-                {
-                    await SendError(update, e);
-                }
-            }
-            catch (Exception e)
-            {
-                switch (e)
-                {
-                    case ArgumentOutOfRangeException:
-                        LogOut(update.Type);
-                        break;
-                    case ApiRequestException:
-                        LogOutWarning(e.Message);
-                        break;
-                    default:
-                        LogOutError(e);
-                        break;
-                }
-            }
-        }
-
-        private static async Task SendError(Update update, Exception exception)
-        {
-            try
-            {
-                var context = new BotDatabaseContext();
-                var userDetails = update.Type switch
-                {
-                    UpdateType.Message => update.Message!.From,
-                    UpdateType.CallbackQuery => update.CallbackQuery!.From,
-                    UpdateType.ChosenInlineResult => update.ChosenInlineResult!.From,
-                    UpdateType.MyChatMember => update.MyChatMember!.From,
-                    UpdateType.InlineQuery => update.InlineQuery!.From,
-                    UpdateType.PreCheckoutQuery => update.PreCheckoutQuery!.From,
-                    _ => null
-                };
-                if (userDetails == null) return;
-                var user = await context.Users.FindUser(userDetails);
-                if (user.IsBlocked) return;
-                await new SayError(user, context, exception).PrepareAndExecute();
-            }
-            catch (Exception e)
-            {
-                LogOutError(exception);
-                LogOutError(e);
-            }
-        }
-
-        public static Task HandleErrorAsync(ITelegramBotClient client, Exception e, CancellationToken ct)
-        {
-            switch (e)
-            {
-                case ApiRequestException:
-                    LogOutWarning(e.Message);
-                    break;
-                default:
-                    LogOutError(e);
-                    break;
-            }
-
-            return Task.CompletedTask;
-        }
-
         public static async Task<int> EditMessage(
             long chatId,
             int messageId,
@@ -266,17 +173,18 @@ namespace CardCollector.Controllers
         {
             if (user.IsBlocked) return -1;
             var module = user.Session.GetModule<ShopModule>();
-            var token = module.SelectedProvider switch
+            /*var token = module.SelectedProvider switch
             {
                 "Сбербанк" => AppSettings.SberbankToken,
                 "ЮКасса" => AppSettings.YouKassaToken,
+                "ПСБ" => AppSettings.PSBToken,
                 _ => ""
-            };
+            };*/
 
             try
             {
                 var result = await Bot.Client.SendInvoiceAsync(user.ChatId, title, description, payload,
-                    token, currency.ToString(), prices, replyMarkup: keyboard, disableNotification: true);
+                    AppSettings.PSBToken, currency.ToString(), prices, replyMarkup: keyboard, disableNotification: true);
                 user.Messages.ChatMessages.Add(result.MessageId);
                 return result.MessageId;
             }
---
+++ CardCollector/Controllers/UpdateController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CardCollector.Commands;
+using CardCollector.Commands.CallbackQueryHandler;
+using CardCollector.Commands.ChosenInlineResultHandler;
+using CardCollector.Commands.InlineQueryHandler;
+using CardCollector.Commands.MessageHandler;
+using CardCollector.Commands.MyChatMemberHandler;
+using CardCollector.Commands.PreCheckoutQueryHandler;
+using CardCollector.Database;
+using CardCollector.Database.EntityDao;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace CardCollector.Controllers
+{
+    using static Logs;
+
+    public static class UpdateController
+    {
+        public static async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken ct)
+        {
+            try
+            {
+                var handler = await BuildHandler(update);
+                await handler.PrepareAndExecute();
+            }
+            catch (Exception e)
+            {
+                await SendError(update, e);
+            }
+        }
+
+        private static async Task<HandlerModel> BuildHandler(Update update)
+        {
+            return update.Type switch
+            {
+                UpdateType.Message => await MessageHandler.Factory(update),
+                UpdateType.CallbackQuery => await CallbackQueryHandler.Factory(update),
+                UpdateType.MyChatMember => await MyChatMemberHandler.Factory(update),
+                UpdateType.InlineQuery => await InlineQueryHandler.Factory(update),
+                UpdateType.ChosenInlineResult => await ChosenInlineResultHandler.Factory(update),
+                UpdateType.PreCheckoutQuery => await PreCheckoutQueryHandler.Factory(update),
+                _ => new IgnoreHandler()
+            };
+        }
+
+        private static async Task SendError(Update update, Exception exception)
+        {
+            try
+            {
+                var context = new BotDatabaseContext();
+                var userDetails = update.Type switch
+                {
+                    UpdateType.Message => update.Message!.From,
+                    UpdateType.CallbackQuery => update.CallbackQuery!.From,
+                    UpdateType.ChosenInlineResult => update.ChosenInlineResult!.From,
+                    UpdateType.MyChatMember => update.MyChatMember!.From,
+                    UpdateType.InlineQuery => update.InlineQuery!.From,
+                    UpdateType.PreCheckoutQuery => update.PreCheckoutQuery!.From,
+                    _ => null
+                };
+                if (userDetails == null) return;
+                var user = await context.Users.FindUser(userDetails);
+                if (user.IsBlocked) return;
+                
+                await new SayError()
+                    .SetException(exception)
+                    .Init(user, context, update)
+                    .PrepareAndExecute();
+            }
+            catch (Exception e)
+            {
+                LogOutError(exception);
+                LogOutError(e);
+            }
+        }
+
+        public static Task HandleErrorAsync(ITelegramBotClient client, Exception e, CancellationToken ct)
+        {
+            switch (e)
+            {
+                case ApiRequestException:
+                    LogOutWarning(e.Message);
+                    break;
+                default:
+                    LogOutError(e);
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
